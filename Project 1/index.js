535d81d4d0aabb45d7efabf8563e0cea68404b49
Sachinbhujel <90704616+Sachinbhujel@users.noreply.github.com>
2023-07-28 11:07:46 +0000

    hiii

--- Project 1/index.js
+++ Project 1/index.js
@@ -1,1 +1,9 @@
-alert(0);
+function next(){
+    document.getElementById("next").style.display = "none"
+    document.getElementById("back").style.display = "block"
+}
+
+function back(){
+    document.getElementById("next").style.display = "block"
+    document.getElementById("back").style.display = "none"
+}
