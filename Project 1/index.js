57a68c2ede25d6782907df038731b2bab1aa0e87
Sachinbhujel <90704616+Sachinbhujel@users.noreply.github.com>
2023-07-29 10:42:00 +0000

    ohh

--- Project 1/index.js
+++ Project 1/index.js
@@ -7,3 +7,7 @@ function back(){
     document.getElementById("next").style.display = "block"
     document.getElementById("back").style.display = "none"
 }
+
+function log_off(){
+    alert("Are you sure you want to exit from this account?");
+}
