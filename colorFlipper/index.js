dffd0c89cdfb72e01241aef671f89fd9a69d950f
Sachinbhujel <90704616+Sachinbhujel@users.noreply.github.com>
2023-09-12 18:14:21 +0000

    hiii

---
+++ colorFlipper/index.js
@@ -0,0 +1,7 @@
+var color = ["red", "blue", "green", "coral", "pink", "purple"]
+var btn = document.getElementById("btn");
+var color = document.getElementsByClassName("colors");
+
+
+function click(){
+}
