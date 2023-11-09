1bc0438067253ef7540fe9eaabaef3f06e4888a3
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2023-11-09 23:59:58 +0530

    Update index.js

--- projects/windows11/index.js
+++ projects/windows11/index.js
@@ -5,6 +5,7 @@ let startMenu = document.getElementsByClassName("startmenu")[0];
 function allApp(){
   if(startMenu.style.bottom == "60px"){
     startMenu.style.bottom = "-655px"
+    startMenu.style.display = "block"
   } else{
     startMenu.style.bottom = "60px"
   }
