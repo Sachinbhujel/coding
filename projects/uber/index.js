5a32dfdb8f3f009a63d4e1488f975b1d4ffc26f5
Sachin Bhujel <sachinbhujel909@gmail.com>
2023-11-11 10:55:03 +0000

    ok

---
+++ projects/uber/index.js
@@ -0,0 +1,9 @@
+function ride(){
+    document.getElementById("destiny1").style.display = "block";
+    document.getElementById("destiny2").style.display = "none";
+}
+
+function driver(){
+    document.getElementById("destiny2").style.display = "block";
+    document.getElementById("destiny1").style.display = "none";
+}
