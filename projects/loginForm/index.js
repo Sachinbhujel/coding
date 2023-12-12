55d338acf8ac50c03128258ceb2a5c1def954796
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2023-12-12 23:43:05 +0530

    Create index.js

---
+++ projects/loginForm/index.js
@@ -0,0 +1,22 @@
+function show() {
+  let password = document.getElementById("password");
+  //let data = password.value;
+  password.type = "text";
+  document.getElementById("show").style.display = "none";
+  document.getElementById("hide").style.display = "block";
+  //password.value = data;
+}
+
+function hide() {
+  let hidden = document.getElementById("hide");
+  password.type = "password";
+  document.getElementById("show").style.display = "block";
+  document.getElementById("hide").style.display = "none";
+}
+
+function logIn() {
+  let email = document.getElementById("email");
+  if (email.value == "" || password.value == "") {
+    document.getElementById("warning").innerHTML = "Fill out this field";
+  }
+}
