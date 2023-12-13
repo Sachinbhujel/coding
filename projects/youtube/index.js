7eacfed6e8c1ee958f380f7e32f65de842f801c7
Sachin Bhujel <sachinbhujel909@gmail.com>
2023-12-13 20:25:19 +0000

    hii

---
+++ projects/youtube/index.js
@@ -0,0 +1,8 @@
+function hideSideBar(){
+    let middleleft = document.getElementById("middle-left");
+    if(middleleft.style.display == "block"){
+        middleleft.style.display = "none";
+    } else{
+        middleleft.style.display = "block";
+    }
+}
