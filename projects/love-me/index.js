e26fb7e77c8caa3586e669453810480b4cbd7ed0
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2023-12-28 14:56:25 +0530

    Create index.js

---
+++ projects/love-me/index.js
@@ -0,0 +1,25 @@
+//let buttonHeight = 50;
+//let buttonWeight = 150;
+ 
+const b = document.querySelector("button.button--no")
+b.addEventListener("mouseover", hoverMouse)
+
+function hoverMouse(){
+  const w = Math.floor(Math.random() * 22) + 1;
+  const h = Math.floor(Math.random() * 50) + 1;
+  console.log(w);
+  console.log(h);
+  
+  b.style.left = w+"%";
+  b.style.top = h+"vh";
+}
+
+function yes(){
+  let answer = document.getElementById("answer");
+  answer.innerHTML = "I LOVE YOU TOO!!"
+}
+
+function no(){
+  let answer = document.getElementById("answer");
+  answer.innerHTML = "BUT, I STILL LOVE YOU!!"
+}
