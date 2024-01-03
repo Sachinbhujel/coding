3567f1419bdbcdb82067dfd510162f17b424fbaf
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-01-03 22:56:33 +0530

    Update index.js

--- projects/day03_challenge/index.js
+++ projects/day03_challenge/index.js
@@ -1,4 +1,4 @@
-var colors = ["red", "purple", "coral", "lightgreen", "#606C38", "#FEFAE0", "#DDA15E", "#CDB4DB", "#FFAFCC", "#A2D2FF", "#023047", "#FFB703", "#EDEDE9", "#E3D5CA", "#023E8A", "#48CAE4", "#EDF2F4", "#3A0CA3", "#8C8071", "cyan", "brown", "#6D597A", "#89FC00", "#008BF8", "#3DCCC7", "#F59CA9", "#A03E99", "#02C39A", "#2E0219", "#97EFE9", "#EDF2F4", "#FF499E", "#372B2B"];
+var colors = ["red", "purple", "coral", "lightgreen", "#606C38", "#FEFAE0", "#DDA15E", "#CDB4DB", "#FFAFCC", "#A2D2FF", "#023047", "#FFB703", "#EDEDE9", "#E3D5CA", "#023E8A", "#48CAE4", "#EDF2F4", "#3A0CA3", "#8C8071", "cyan", "brown", "#6D597A", "#89FC00", "#008BF8", "#3DCCC7", "#F59CA9", "#A03E99", "#02C39A", "#2E0219", "#97EFE9", "#EDF2F4", "#FF499E", "#372B2B"]
 var btn = document.getElementById('btn');
 var color = document.querySelector(".color");
 
@@ -12,4 +12,3 @@ btn.addEventListener("click", function(){
 function getRandomNumber(){
   return Math.floor(Math.random() * colors.length);
 }
-
