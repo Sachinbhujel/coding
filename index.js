7fb3dadf3eb1b364bcbf10d9dc63bc330c45569a
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-01-03 14:03:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-let counterContainer = document.querySelector(".website-counter");
-let visitCount = localStorage.getItem("page_view");
 
-// Increment the visit count
-visitCount = Number(visitCount) + 1;
-
-// Update the localStorage with the new visit count
-localStorage.setItem("page_view", visitCount);
-
-// Display the visit count on the page
-//localStorage.removeItem("page_view")
-counterContainer.innerHTML = visitCount;
