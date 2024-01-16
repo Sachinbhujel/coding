a6ee50ec116cee4aa9e9ab6b859b5d2a1944b407
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-01-16 16:58:37 +0530

    Create index.js

---
+++ projects/day07_challenge/index.js
@@ -0,0 +1,13 @@
+function hide(){
+  document.getElementById("main").style.display = "none";
+  document.getElementById("main2").style.display = "block";
+}
+
+function show(){
+   document.getElementById("main").style.display = "block"; 
+   document.getElementById("main2").style.display = "none";
+}
+
+function warning(){
+  alert("Continue browsing the Website!")
+}
