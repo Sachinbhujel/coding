23fd24af0fd9719039bc9350b61133bb8c857506
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-01-16 15:29:06 +0530

    Create index.js

---
+++ projects/masonryImageGallery/index.js
@@ -0,0 +1,14 @@
+let img_show_div = document.getElementById("img_show_div");
+let img = document.getElementById("img");
+
+function imgs(link){
+  img_show_div.style.display = "flex";
+  img.src = link;
+  document.body.style.background = "grey";
+}
+
+function close_img(){
+  let img_show_div = document.getElementById("img_show_div");
+    img_show_div.style.display = "none";
+    document.body.style.background = "white";
+}
