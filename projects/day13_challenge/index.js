c4eb494575163904aab250200392863d45ef9990
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-01-22 22:35:21 +0530

    Create index.js

---
+++ projects/day13_challenge/index.js
@@ -0,0 +1,23 @@
+function showShoes(){
+  document.getElementById("img2").style.display = "flex";
+  document.getElementById("img1").style.display = "none";
+}
+
+function hideShoes(){
+  document.getElementById("img1").style.display = "flex";
+  document.getElementById("img2").style.display = "none";
+}
+
+function showCart(){
+  let mainDiv = document.getElementById("mainDiv");
+  let showCart_div = document.getElementById("showCart_div");
+ showCart_div.style.display = "block";
+ mainDiv.style.display = "none"
+}
+
+function exitBtn(){
+  let mainDiv = document.getElementById("mainDiv");
+  let showCart_div = document.getElementById("showCart_div");
+ showCart_div.style.display = "none";
+ mainDiv.style.display = "block";
+}
