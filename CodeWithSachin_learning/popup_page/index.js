dadd2f395fc8868750f1640144ab59e877def198
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-03-25 15:53:56 +0530

    Delete CodeWithSachin_learning directory

--- CodeWithSachin_learning/popup_page/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function() {
-    document.getElementById("offer-popup").style.display = "block";
-  }, 5000);
-
-  document.getElementById("cancel-btn").addEventListener("click", function() {
-    document.getElementById("offer-popup").style.display = "none";
-  });
-});
